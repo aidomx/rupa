6e32a5d4c01e196387e17114cab77e16c3925efc
wadahkode <mvp.dedefilaras@gmail.com>
2025-09-12 08:28:53 +0000

    Refactor: membersihkan dan memindahkan parser

--- include/rupa/parser/expression.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "rupa/package.h"
-
-extern int isStandaloneToken(Token *t, int pos);
--- include/rupa/parser/package.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "rupa/package.h"
-
-#include "rupa/parser/assignment.h"
-#include "rupa/parser/expression.h"
-#include "rupa/parser/operator.h"
-#include "rupa/parser/structure.h"
